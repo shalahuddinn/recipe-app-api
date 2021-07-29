4da9363b019d7b4013735a48991b77db92aec44a
Shalahuddin A <shalahuddinnn@yahoo.com>
2021-07-29 08:37:14 +0700

    Added custom recipe detail view.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -40,3 +40,9 @@ class RecipeSerializer(serializers.ModelSerializer):
         )
         # Prevent update id by user
         read_only_fields = ('id',)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serialize a recipe detail"""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
