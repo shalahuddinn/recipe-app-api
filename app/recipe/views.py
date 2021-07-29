7ba0fdd441bc63869583e762f589621de4fd3222
Shalahuddin A <shalahuddinnn@yahoo.com>
2021-07-29 14:58:56 +0700

    Added recipe, tags, ingredients filtering feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -18,7 +18,15 @@ class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
 
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+        return queryset.filter(
+                user=self.request.user
+            ).order_by('-name').distinct()
 
     def perform_create(self, serializer):
         """Create a new object"""
@@ -44,9 +52,25 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
+    # python convention to name with _ prefix for an intended private function
+    def _params_to_ints(self, queryString):
+        """Convert list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in queryString.split(',')]
+
     def get_queryset(self):
         """Retrieve the recipes for the authenticated user"""
-        return self.queryset.filter(user=self.request.user)
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            # tags is a foreign key
+            queryset = queryset.filter(tags__id__in=tag_ids)
+        if ingredients:
+            ingredients_ids = self._params_to_ints(ingredients)
+            # ingredients is a foreign key
+            queryset = queryset.filter(ingredients__id__in=ingredients_ids)
+        return queryset.filter(user=self.request.user)
 
     def get_serializer_class(self):
         """Return appropriate serializer class"""
